2ed505f24c95f533f72ee97e946106cbec8642b4
local-fish <Andrew.arinardi@gmail.com>
2026-04-27 11:21:02 +0700

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+	};
+
+	outputs = { self, nixpkgs }@inputs:
+	let
+		system = "x86_64-linux";
+		
+		pkgs = import inputs.nixpkgs{
+			inherit system;
+			config.allowUnfree = true;
+			config.android_sdk.accept_license = true;
+		};
+		androidComposition = pkgs.androidenv.composeAndroidPackages {
+			buildToolsVersions = [ "35.0.0" "34.0.0" "33.0.1" ];
+			platformVersions = [ "35" "34" "33" "28" ];
+			abiVersions = [ "x86_64" "armeabi-v7a" "arm64-v8a" ];
+			includeNDK = true;
+			ndkVersions = [ "26.3.11579264" ];
+			cmakeVersions = [ "3.22.1" ];
+		};
+		androidSdk = androidComposition.androidsdk;
+	in 
+	{
+		devShells.${system}.default = pkgs.mkShell rec {
+			name="Leaper";
+			ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+			GRADLE_OPTS = "-Dorg.gradle.project.android.aapt2FromMavenOverride=${androidSdk}/libexec/android-sdk/build-tools/34.0.0/aapt2";
+			packages = with pkgs; [
+					flutter332
+					androidSdk
+					jdk17
+					nodejs
+				];
+			shellHook="tmux -L Leaper new-session -A -s Leaper";
+			};
+		};
+	}
