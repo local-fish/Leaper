803bb1a05052bafe546b02b299dc0bb81a666444
local-fish <Andrew.arinardi@gmail.com>
2026-05-04 20:41:52 +0700

    Refactor: Moved Main Layout to Pages

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:leaper/core/components/main_layout.dart';
+import 'package:leaper/pages/main_layout.dart';
 import 'package:leaper/main.dart';
 import 'package:leaper/pages/dashboard/schedule.dart';
 import 'package:leaper/pages/login_page.dart';
