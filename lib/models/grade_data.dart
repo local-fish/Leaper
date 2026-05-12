a503c214aa4fcaadbe4cd1631d12ecf64dd7e58d
local-fish <Andrew.arinardi@gmail.com>
2026-05-13 01:35:33 +0700

    Refactor Removed Unessecary Comments

--- lib/models/grade_data.dart
+++ lib/models/grade_data.dart
@@ -1,6 +1,5 @@
 import 'package:json_annotation/json_annotation.dart';
-
-part 'grade_data.g.dart'; // this file gets auto-generated
+part 'grade_data.g.dart';
 
 @JsonSerializable()
 class GradeData {
