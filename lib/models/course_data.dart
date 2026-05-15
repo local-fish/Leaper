8011778082a751cd577e8ea7cb5d6f98350c864d
local-fish <Andrew.arinardi@gmail.com>
2026-05-15 23:27:08 +0700

    Feat: Added Mapping for Course Session Data

--- lib/models/course_data.dart
+++ lib/models/course_data.dart
@@ -44,3 +44,47 @@ class CourseStudentData {
       _$CourseStudentDataFromJson(json);
   Map<String, dynamic> toJson() => _$CourseStudentDataToJson(this);
 }
+
+@JsonSerializable()
+class CourseSessionData {
+  String topic;
+  int id;
+  int sessionNo;
+  DateTime startTime;
+  DateTime endTime;
+  String location;
+  List<FileData>? files;
+
+  CourseSessionData({
+    required this.topic,
+    required this.id,
+    required this.sessionNo,
+    required this.startTime,
+    required this.endTime,
+    required this.location,
+    this.files,
+  });
+  static List<CourseSessionData> fromJsonList(List<dynamic> json) {
+    return json.map((e) => CourseSessionData.fromJson(e)).toList();
+  }
+
+  factory CourseSessionData.fromJson(Map<String, dynamic> json) =>
+      _$CourseSessionDataFromJson(json);
+  Map<String, dynamic> toJson() => _$CourseSessionDataToJson(this);
+}
+
+@JsonSerializable()
+class FileData {
+  String id;
+  String name;
+  int size;
+
+  FileData({required this.id, required this.name, required this.size});
+  static List<FileData> fromJsonList(List<dynamic> json) {
+    return json.map((e) => FileData.fromJson(e)).toList();
+  }
+
+  factory FileData.fromJson(Map<String, dynamic> json) =>
+      _$FileDataFromJson(json);
+  Map<String, dynamic> toJson() => _$FileDataToJson(this);
+}
--- lib/models/course_data.g.dart
+++ lib/models/course_data.g.dart
@@ -36,3 +36,39 @@ Map<String, dynamic> _$CourseStudentDataToJson(CourseStudentData instance) =>
       'email': instance.email,
       'role': instance.role,
     };
+
+CourseSessionData _$CourseSessionDataFromJson(Map<String, dynamic> json) =>
+    CourseSessionData(
+      topic: json['topic'] as String,
+      id: (json['id'] as num).toInt(),
+      sessionNo: (json['sessionNo'] as num).toInt(),
+      startTime: DateTime.parse(json['startTime'] as String),
+      endTime: DateTime.parse(json['endTime'] as String),
+      location: json['location'] as String,
+      files: (json['files'] as List<dynamic>?)
+          ?.map((e) => FileData.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$CourseSessionDataToJson(CourseSessionData instance) =>
+    <String, dynamic>{
+      'topic': instance.topic,
+      'id': instance.id,
+      'sessionNo': instance.sessionNo,
+      'startTime': instance.startTime.toIso8601String(),
+      'endTime': instance.endTime.toIso8601String(),
+      'location': instance.location,
+      'files': instance.files,
+    };
+
+FileData _$FileDataFromJson(Map<String, dynamic> json) => FileData(
+  id: json['id'] as String,
+  name: json['name'] as String,
+  size: (json['size'] as num).toInt(),
+);
+
+Map<String, dynamic> _$FileDataToJson(FileData instance) => <String, dynamic>{
+  'id': instance.id,
+  'name': instance.name,
+  'size': instance.size,
+};
