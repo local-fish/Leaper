e2f9646a90ccb75ed1b59e06f11805fbab1cc553
local-fish <Andrew.arinardi@gmail.com>
2026-05-17 22:42:56 +0700

    Feat: Added Logout

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:leaper/core/components/scaffold_background.dart';
+import 'package:leaper/providers/auth_provider.dart';
+import 'package:leaper/providers/user_info_provider.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends ConsumerWidget {
   const Profile({super.key});
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return ScaffoldBackground(
-      child: Center(
-        child: Text(
-          textAlign: TextAlign.center,
-          "This is a template page. I haven't implemented anything, so have a duck 🦆",
+      child: ConstrainedBox(
+        constraints: BoxConstraints(minWidth: double.infinity),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                final nav = Navigator.of(context);
+                await ref.read(userInfoProvider.notifier).logout();
+                await ref.read(authProvider.notifier).logout();
+                nav.pushReplacementNamed('/');
+              },
+              child: Text("Log Out"),
+            ),
+          ],
         ),
       ),
     );
