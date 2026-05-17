9b5f9d20a92f52eae91f3d133190ad8c586f6126
local-fish <Andrew.arinardi@gmail.com>
2026-05-17 14:34:11 +0700

    Feat: Moved Sensitive Auth Token to Secure Storage

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,27 +1,27 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const _storage = FlutterSecureStorage();
 
 final authProvider = AsyncNotifierProvider<AuthNotifier, String?>(
   AuthNotifier.new,
 );
 
 class AuthNotifier extends AsyncNotifier<String?> {
-  late final SharedPreferences _perfs;
   @override
   Future<String?> build() async {
-    _perfs = await SharedPreferences.getInstance();
-    return _perfs.getString('token');
+    return await _storage.read(key: 'token');
   }
 
   Future<void> login(String token) async {
     await future;
-    await _perfs.setString('token', token);
+    await _storage.write(key: 'token', value: token);
     state = AsyncData(token);
   }
 
   Future<void> logout() async {
     await future;
-    await _perfs.remove('token');
+    await _storage.delete(key: 'token');
     state = AsyncData(null);
   }
 }
