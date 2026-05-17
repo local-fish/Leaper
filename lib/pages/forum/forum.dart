2f5079d0f2fc4cac657831e812b5c06b10b8006d
local-fish <Andrew.arinardi@gmail.com>
2026-05-17 14:48:25 +0700

    Feat: Added Forum List Items

--- lib/pages/forum/forum.dart
+++ lib/pages/forum/forum.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:leaper/core/components/back_nav_heading.dart';
+import 'package:leaper/core/components/heading_card.dart';
 import 'package:leaper/core/components/scaffold_background.dart';
+import 'package:leaper/core/styles/text_styles/font_sizes.dart';
 import 'package:leaper/models/forum_data.dart';
 import 'package:leaper/providers/auth_provider.dart';
 
@@ -67,10 +71,70 @@ class _ForumState extends ConsumerState<Forum> {
                 );
               }
               final data = snapshot.data!;
-              return Column(children: data.map((x) => Text(x.title)).toList());
+              data.sort((a, b) => b.time.compareTo(a.time));
+              return Expanded(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      children: data
+                          .expand(
+                            (x) => {
+                              ForumListCard(item: x),
+                              SizedBox(height: 8),
+                            },
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ),
+              );
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ForumListCard extends StatelessWidget {
+  final ForumData item;
+  const ForumListCard({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return HeadingCard(
+      heading: Padding(
+        padding: EdgeInsets.only(left: 8, right: 8),
+        child: Text(
+          item.title,
+          style: GoogleFonts.montserrat(
+            fontSize: FontSizes.small,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      content: ConstrainedBox(
+        constraints: BoxConstraints(minWidth: double.infinity),
+        child: Table(
+          columnWidths: const <int, TableColumnWidth>{
+            0: IntrinsicColumnWidth(),
+            1: FlexColumnWidth(),
+          },
+          children: <TableRow>[
+            TableRow(
+              children: [
+                Text("Posted on "),
+                Text(
+                  ": ${DateFormat('dd MMM yyyy • HH:mm:ss').format(item.time)}",
+                ),
+              ],
+            ),
+            TableRow(
+              children: [Text("Posted by"), Text(": ${item.user.name}")],
+            ),
+          ],
+        ),
       ),
     );
   }
