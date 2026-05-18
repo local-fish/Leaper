c2d80f58e50db1cf9caa14280772dd1e5b7cd4b5
local-fish <Andrew.Arinardi@gmail.com>
2026-05-18 09:58:55 +0700

    Feat: Added Logo in Login Screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -75,82 +75,86 @@ class _LoginPageState extends ConsumerState<LoginPage> {
         child: Column(
           children: [
             Expanded(
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(left: 32, right: 32),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
+              child: Padding(
+                padding: EdgeInsets.only(left: 32, right: 32),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset(
+                      'assets/images/icon.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: FontSizes.veryLarge,
+                        color: Color(0xFF787880),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 12)),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFFFFFF),
+                        borderRadius: BorderRadius.circular(32),
+                        boxShadow: [
+                          BoxShadow(color: Color(0x0D000000), blurRadius: 24),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 12, right: 12),
+                        child: TextFormField(
+                          controller: _usernameController,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.person),
+                            hintText: "Username or Email",
+                            border: InputBorder.none,
+                            hintStyle: InputStyle.hintText,
+                          ),
+                          style: InputStyle.inputText,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 8)),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFFFFFFF),
+                        borderRadius: BorderRadius.circular(32),
+                        boxShadow: [
+                          BoxShadow(color: Color(0x0D000000), blurRadius: 24),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 12, right: 12),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            icon: Icon(Icons.lock),
+                            hintText: "Password",
+                            border: InputBorder.none,
+                            hintStyle: InputStyle.hintText,
+                          ),
+                          style: InputStyle.inputText,
+                          obscureText: true,
+                        ),
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 8)),
+                    ElevatedButton(
+                      onPressed: handleLogin,
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 96,
+                          vertical: 8,
+                        ),
+                      ),
+                      child: Text(
                         "Login",
-                        style: TextStyle(
-                          fontSize: FontSizes.veryLarge,
-                          color: Color(0xFF787880),
-                        ),
+                        style: TextStyle(fontSize: FontSizes.medium),
                       ),
-                      Padding(padding: EdgeInsets.only(top: 12)),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xFFFFFFFF),
-                          borderRadius: BorderRadius.circular(32),
-                          boxShadow: [
-                            BoxShadow(color: Color(0x0D000000), blurRadius: 24),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 12, right: 12),
-                          child: TextFormField(
-                            controller: _usernameController,
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.person),
-                              hintText: "Username or Email",
-                              border: InputBorder.none,
-                              hintStyle: InputStyle.hintText,
-                            ),
-                            style: InputStyle.inputText,
-                          ),
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(top: 8)),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Color(0xFFFFFFFF),
-                          borderRadius: BorderRadius.circular(32),
-                          boxShadow: [
-                            BoxShadow(color: Color(0x0D000000), blurRadius: 24),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 12, right: 12),
-                          child: TextFormField(
-                            controller: _passwordController,
-                            decoration: InputDecoration(
-                              icon: Icon(Icons.lock),
-                              hintText: "Password",
-                              border: InputBorder.none,
-                              hintStyle: InputStyle.hintText,
-                            ),
-                            style: InputStyle.inputText,
-                            obscureText: true,
-                          ),
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(top: 8)),
-                      ElevatedButton(
-                        onPressed: handleLogin,
-                        style: ElevatedButton.styleFrom(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 96,
-                            vertical: 8,
-                          ),
-                        ),
-                        child: Text(
-                          "Login",
-                          style: TextStyle(fontSize: FontSizes.medium),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
